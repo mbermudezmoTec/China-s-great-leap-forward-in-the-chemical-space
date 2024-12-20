d70980adc7d19ad7b6007f3c438bcbb62c2b61cd
guillermorestrepo <32931810+khemeia@users.noreply.github.com>
2024-12-20 14:21:24 +0100

    Update 11_dumping_Dimensions_byTitles.py

--- scripts/preprocessing/11_dumping_Dimensions_byTitles.py
+++ scripts/preprocessing/11_dumping_Dimensions_byTitles.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
+#Script to dump data from Dimensions, searches are made by title
 
 import dimcli
 import pandas as pd
 import json
 
 # you can pass explicitly your login details as arguments
-dimcli.login(key="327B256FE7C7435994F18BCC209C8BD4", endpoint="https://app.dimensions.ai")
+dimcli.login(key=" ", endpoint="https://app.dimensions.ai")
 
 dsl = dimcli.Dsl()
 
 reacrxns=pd.read_csv('paren_titles_Hydra_3.tsv',sep='\t',dtype=str,header=None)
-#reacrxns=pd.read_csv('t1',sep='\t',dtype=str,header=None)
 p5=reacrxns.head()
 lp5=p5[0].tolist()
 col_one_list = reacrxns[0].tolist()
