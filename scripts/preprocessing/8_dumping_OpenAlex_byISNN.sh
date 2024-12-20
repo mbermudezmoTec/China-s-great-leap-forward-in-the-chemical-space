0d45e1f367b2311f28210be96b97e5940d7c6d28
guillermorestrepo <32931810+khemeia@users.noreply.github.com>
2024-12-20 14:15:21 +0100

    Update 8_dumping_OpenAlex_byISNN.sh

--- scripts/preprocessing/8_dumping_OpenAlex_byISNN.sh
+++ scripts/preprocessing/8_dumping_OpenAlex_byISNN.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+#Script to dump data from OpenAlex.  Searches are made by authors, titles, pages, years and ISSN.
 
 for i in $(seq 1 130000);
 
 do echo $i 
-   a=$(sed -n $i'{p;q}' file.txt )
+   a=$(sed -n $i'{p;q}' file.txt ) #file.txt was created by 7_creating_prompt_ISSN.py
    wget $a
    cat work* >> out_try_2.txt
    rm work*
