d9e6b74817c1d6796bc7052a8779802f3026fd86
guillermorestrepo <32931810+khemeia@users.noreply.github.com>
2024-12-20 14:25:51 +0100

    Update 13_processing_Dimensions_Data.py

--- scripts/preprocessing/13_processing_Dimensions_Data.py
+++ scripts/preprocessing/13_processing_Dimensions_Data.py
@@ -1,29 +1,27 @@
 #!/usr/bin/env python3
+#This script processes raw data dumped from Dimensions and returns author names and their affiliations for authors of papers reporting substances in Reaxys.
 
 import json
 import ast
 
 with open('cuncho_dois_Dimensions_2.tsv') as file:
-#with open('t1') as file:
     for line in file:    
         json_data = ast.literal_eval(str(line))                
-        if 'publications' in json_data: #publications es una lista de un unico elemento
+        if 'publications' in json_data: #publications is a list of a single element
             if(len(json_data['publications'])>0 and 'publications' in json_data):
                 a=json_data['publications'][0]    
                 if('doi' in a):
                     print(a['doi'],end="\t")
-                    if 'authors' in a:# 'authors' es una lista donde cada autor tiene un diccionario: dict_keys(['affiliations', 'corresponding', 'current_organization_id', 'first_name', 'last_name', 'orcid', 'raw_affiliation', 'researcher_id'])
+                    if 'authors' in a:# 'authors' is a list, where each authors has a dictionary: dict_keys(['affiliations', 'corresponding', 'current_organization_id', 'first_name', 'last_name', 'orcid', 'raw_affiliation', 'researcher_id'])
                         print(len(a['authors']),end='\t')
                         for i in range(len(a['authors'])):
                             print(a['authors'][i]['first_name']+" "+a['authors'][i]['last_name'],end=':')
                         print(end='\t')
                         for i in range(len(a['authors'])):
                             if len(a['authors'][i]['affiliations'])>0:
-                                print(a['authors'][i]['affiliations'][0]['country_code'],end=':')#'affiliations' es una lista porque hay autores que tienen mas de una adscripcion. Se va a tomar UNICAMENTE LA PRIMERA ADSCRIPCION
+                                print(a['authors'][i]['affiliations'][0]['country_code'],end=':')#'affiliations' is a list, for there are authors with more than one affiliation. We only take the first affiliation.
                         print(end='\n')
                     else:
                         print('\t\t',end='\n')
                 else:
-                    print(end='\n')
-                    
-                        
+                    print(end='\n')        
